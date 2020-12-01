a8c3acad8b31c62edfa763dab889f4b8b0a3732b
Ndn1618 <ndilbar1618@gmail.com>
2020-12-01 20:53:39 +0500

    Add utils js

---
+++ js/utils.js
@@ -0,0 +1,18 @@
+var $ = function (selector, node = document) {
+  return node.querySelector(selector);
+};
+
+var $$ = function (selector, node = document) {
+  return node.querySelectorAll(selector);
+};
+
+var createElement = function (tagName, className, text) {
+  var element = document.createElement(tagName);
+  element.setAttribute('class', className);
+
+  if (text) {
+    element.textContent = text;
+  }
+
+  return element;
+};
