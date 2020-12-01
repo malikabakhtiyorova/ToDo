252ac676fa8bd9c01f03960cfbe48eae9d91ca34
Ndn1618 <ndilbar1618@gmail.com>
2020-12-01 21:02:19 +0500

    Choosing DOM elements

--- js/script.js
+++ js/script.js
@@ -4,4 +4,7 @@ var todos = JSON.parse(localStorage.getItem('todos')) || [];
 var todoId = Number(localStorage.getItem('todoId'));
 
 //Chosing elements
-
+var elTodoForm = $_('.js-task-form');
+var elTodoInput = $_('.js-task-input', elTodoForm);
+var elTodoList = $_('.js-tasks-list');
+var elTodoTamplate = $('#task-template').content;
