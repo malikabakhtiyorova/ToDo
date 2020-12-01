d90d68a1b2579c7989a440a94fe72b38f99d0163
Ndn1618 <ndilbar1618@gmail.com>
2020-12-01 20:49:26 +0500

    Take todos array and todoId from local storage

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// Take todos array and todosId from local storage
+var todos = JSON.parse(localStorage.getItem('todos')) || [];
+
+var todoId = Number(localStorage.getItem('todoId'));
+
+//Chosing elements
+
